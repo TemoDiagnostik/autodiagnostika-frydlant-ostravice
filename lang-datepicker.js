8bfea64d9d566950e972175cfa91ffd3f086b30f
TemoDiagnostik <temo44t@yahoo.com>
2025-05-09 14:20:05 +0200

    Delete lang-datepicker.js

--- lang-datepicker.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  let lang = document.documentElement.lang || 'cs';
-  flatpickr("#datepicker", {
-    dateFormat: "d.m.Y",
-    locale: lang === "de" ? "de" : lang === "en" ? "en" : "cs",
-    disableMobile: true
-  });
-});
